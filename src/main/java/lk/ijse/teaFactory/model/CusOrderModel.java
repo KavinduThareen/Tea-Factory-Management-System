4c224afc46cc60748f38381b7945379954bf06e1
thareen <kavindutharin@gmail.com>
2023-12-27 19:37:13 +0530

    new commit

--- src/main/java/lk/ijse/teaFactory/model/CusOrderModel.java
+++ src/main/java/lk/ijse/teaFactory/model/CusOrderModel.java
@@ -34,7 +34,7 @@ public class CusOrderModel {
         }
         return dtoList;
     }
-
+/*
     public static boolean deleteItem(String id) throws SQLException {
         Connection connection = DbConnection.getInstance().getConnection();
 
@@ -45,6 +45,8 @@ public class CusOrderModel {
         return pstm.executeUpdate() > 0;
     }
 
+
+ */
     public static String generateNextOrderId() throws SQLException {
         Connection connection = DbConnection.getInstance().getConnection();
 
